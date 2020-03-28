10e108af2c62c45646d4a6619b63f5c712aedca6
Yogesh Aggarwal <developeryogeshgit@gmail.com>
2020-03-28 20:03:44 +0530

    Basic design for context menu

--- src/app/components/context-menu/context-menu.component.ts
+++ src/app/components/context-menu/context-menu.component.ts
@@ -1,15 +1,37 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'context-menu',
-  templateUrl: './context-menu.component.html',
-  styleUrls: ['./context-menu.component.scss']
+  selector: "context-menu",
+  templateUrl: "./context-menu.component.html",
+  styleUrls: ["./context-menu.component.scss"]
 })
 export class ContextMenuComponent implements OnInit {
-
-  constructor() { }
+  constructor() {}
 
   ngOnInit(): void {
+    document.addEventListener("mousedown", e => {
+      e.preventDefault();
+      const menu = <HTMLElement>(
+        document.getElementsByTagName("context-menu")[0]
+      );
+      if (e.button == 2) {
+        menu.style.opacity = "1";
+        menu.style.top = `${e.pageY}px`;
+        menu.style.left = `${e.pageX}px`;
+      } else if (e.button == 0) {
+        let left = Number(menu.style.left.replace("px", ""));
+        let top = Number(menu.style.top.replace("px", ""));
+        let mouseX = e.pageX;
+        let mouseY = e.pageY;
+        let width = menu.offsetWidth;
+        let height = menu.offsetHeight;
+        if (
+          !(mouseX > left && mouseX < left + width) ||
+          !(mouseY > top && mouseY < top + height)
+        ) {
+          menu.style.opacity = "0";
+        }
+      }
+    });
   }
-
 }
