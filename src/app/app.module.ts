ab02fb0381741d438ca97d8f68d047c6edf8af40
Yogesh Aggarwal <developeryogeshgit@gmail.com>
2020-04-02 21:17:44 +0530

    First Migration to new Apollo API

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,20 @@ import { NgModule } from "@angular/core";
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
 import { HomeComponent } from "./home/home.component";
-import { SidebarComponent } from './sidebar/sidebar.component';
-import { TopbarComponent } from './topbar/topbar.component';
-import { ContextMenuComponent } from './components/context-menu/context-menu.component';
-import { TagComponent } from './components/tag/tag.component';
-import { TrendingComponent } from './trending/trending.component';
-import { AuthorCardComponent } from './components/author-card/author-card.component';
-import { PublicationCardComponent } from './components/publication-card/publication-card.component';
-import { ExploreComponent } from './explore/explore.component';
-import { RavelComponent } from './ravel/ravel.component';
+import { SidebarComponent } from "./sidebar/sidebar.component";
+import { TopbarComponent } from "./topbar/topbar.component";
+import { ContextMenuComponent } from "./components/context-menu/context-menu.component";
+import { TagComponent } from "./components/tag/tag.component";
+import { TrendingComponent } from "./trending/trending.component";
+import { AuthorCardComponent } from "./components/author-card/author-card.component";
+import { PublicationCardComponent } from "./components/publication-card/publication-card.component";
+import { ExploreComponent } from "./explore/explore.component";
+import { RavelComponent } from "./ravel/ravel.component";
+import { GraphQLModule } from "./graphql.module";
+import { HttpClientModule } from "@angular/common/http";
+import { ApolloModule, APOLLO_OPTIONS } from "apollo-angular";
+import { HttpLinkModule, HttpLink } from "apollo-angular-link-http";
+import { InMemoryCache } from "apollo-cache-inmemory";
 
 @NgModule({
   declarations: [
@@ -26,13 +31,30 @@ import { RavelComponent } from './ravel/ravel.component';
     AuthorCardComponent,
     PublicationCardComponent,
     ExploreComponent,
-    RavelComponent,
+    RavelComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    GraphQLModule,
+    ApolloModule,
+    HttpLinkModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [
+    {
+      provide: APOLLO_OPTIONS,
+      useFactory: (httpLink: HttpLink) => {
+        return {
+          cache: new InMemoryCache(),
+          link: httpLink.create({
+            uri: "http:localhost"
+          })
+        };
+      },
+      deps: [HttpLink]
+    }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
--- src/app/data-sync.service.ts
+++ src/app/data-sync.service.ts
@@ -1,6 +1,9 @@
 import { Injectable } from "@angular/core";
 import { ToolsService } from "./tools.service";
 
+import { Apollo } from "apollo-angular";
+import gql from "graphql-tag";
+
 @Injectable({
   providedIn: "root"
 })
@@ -27,187 +30,38 @@ export class DataSyncService extends ToolsService {
     } catch {}
   }
 
-  private async postIdDataToObject(posts, _callback?) {
-    var author;
-    for (let postIndex in posts) {
-      await this.query(
-        `query { getUser(args: {_id: "${posts[postIndex].credit.author}"}) {data {name, profileImg}} }`,
-        reqUser => {
-          //& Published date
-          posts[postIndex].datePublished = this.getCardDate(
-            posts[postIndex].datePublished
-          );
-          //& Updated date
-          posts[postIndex].dateUpdated = this.getCardDate(
-            posts[postIndex].dateUpdated
-          );
-          //& Author
-          author = JSON.parse(reqUser.responseText)["data"]["getUser"]["data"];
-          posts[postIndex].credit.author = author;
-        }
-      );
-      if (Number(postIndex) == posts.length - 1) {
-        if (_callback) _callback(posts);
-        return posts;
-      }
-    }
-  }
-
-  private async getHome(func, data, quantity = 3) {
-    this.query(
-      `query { ${func}(args: {uid: "", quantity: ${quantity}}) {_id, title, thumbnail, tags, datePublished, readTime, credit {author}} }`,
-      req => {
-        this.postIdDataToObject(
-          JSON.parse(req.responseText)["data"][func],
-          r => {
-            switch (data) {
-              case "newRavels":
-                this.newRavels = r;
-                break;
-              case "userRecommendations":
-                this.userRecommendations = r;
-                break;
-
-              default:
-                break;
-            }
-          }
-        );
-      }
-    );
+  private parsePosts(posts) {
+    posts.forEach(post => {
+      post.dateUpdated = this.getCardDate(post.dateUpdated);
+    });
+    return posts;
   }
 
   async getNewRavels() {
-    return await this.getHome("getNewRavels", "newRavels");
+    this.query(
+      `{ getNewRavels { _id, title, thumbnail, readTime, dateUpdated, tags credit { author { _id, data { name, profileImg } } } } }`,
+      ({ responseText }) => {
+        const posts = JSON.parse(responseText)["data"]["getNewRavels"];
+        this.newRavels = this.parsePosts(posts);
+        return this.newRavels;
+      }
+    );
   }
 
   async getUserRecommendations() {
-    return await this.getHome("getUserRecommendations", "userRecommendations");
-  }
-
-  async getTrending() {
     this.query(
-      `query { getTrending {tags, featured {topics, posts}, featuredCreators, date} }`,
-      async req => {
-        const res = JSON.parse(req.responseText)["data"]["getTrending"];
-
-        const categoryPosts = res["featured"]["posts"];
-
-        let final = [];
-        for (let categoryPost of categoryPosts) {
-          let finalCategoryPost = [];
-          for (let post of categoryPost) {
-            await this.query(
-              `query { getPost(args: { _id: "${post}" } ) { _id, title, thumbnail, tags, datePublished, readTime, credit {author} } }`,
-              res => {
-                this.postIdDataToObject(
-                  [JSON.parse(res.responseText)["data"]["getPost"]],
-                  r => {
-                    finalCategoryPost.push(r[0]);
-                  }
-                );
-              }
-            );
-          }
-          final.push(finalCategoryPost);
-        }
-        res["featured"]["posts"] = final;
-
-        let creators = [];
-        for (let creator in res["featuredCreators"]) {
-          await this.query(
-            `query { getFeaturedUser(args:{_id:"${res["featuredCreators"][creator]}"}) {_id, data {name, profileImg, posts {featuredPosts}}} }`,
-            async r => {
-              let fCreator = JSON.parse(r.responseText)["data"][
-                "getFeaturedUser"
-              ];
-              let posts = [];
-              for (let post of fCreator["data"]["posts"]["featuredPosts"]) {
-                await this.query(
-                  ` query { getPost(args: { _id: "${post}" }) { _id, title, thumbnail, tags } } `,
-                  post => {
-                    posts.push(
-                      JSON.parse(post.responseText)["data"]["getPost"]
-                    );
-                  }
-                );
-              }
-              fCreator["data"]["posts"]["featuredPosts"] = posts;
-
-              creators.push(fCreator);
-
-              if (Number(creator) == creator.length - 1) {
-                res["featuredCreators"] = creators;
-                this.trending = res;
-                console.log(this.trending);
-              }
-            }
-          );
-        }
+      `{ getUserRecommendations { _id, title, thumbnail, readTime, dateUpdated, tags credit { author { _id, data { name, profileImg } } } } }`,
+      ({ responseText }) => {
+        const posts = JSON.parse(responseText)["data"][
+          "getUserRecommendations"
+        ];
+        this.userRecommendations = this.parsePosts(posts);
+        return this.userRecommendations;
       }
     );
   }
 
-  async getExplore() {
-    this.query(
-      `query { getExplore { tags, publications, creators, posts } }`,
-      async req => {
-        const data = JSON.parse(req.responseText)["data"]["getExplore"];
+  async getTrending() {}
 
-        //& Getting posts
-        let posts = [];
-        for (let post of data.posts) {
-          await this.query(
-            `query { getPost(args: { _id: "${post}" }) {_id, title, thumbnail, tags, datePublished, readTime, credit {author}} }`,
-            async r => {
-              post = JSON.parse(r.responseText)["data"]["getPost"];
-              await this.postIdDataToObject([post], post => {
-                posts.push(post[0]);
-              });
-            }
-          );
-        }
-        data["posts"] = posts;
-
-        //& Getting publications
-        let publications = [];
-        for (let publication of data.publications) {
-          await this.query(
-            `query { getPublication(args: { _id: "${publication}" }) { _id, name, followers, members, featuredImg } }`,
-            r => {
-              publication = JSON.parse(r.responseText)["data"][
-                "getPublication"
-              ];
-              publication["followers"] = publication["followers"].length;
-              publication["members"] = publication["members"].length;
-              publications.push(publication);
-            }
-          );
-        }
-        data["publications"] = publications;
-
-        //& Getting creators
-        let creators = [];
-        for (let creator of data.creators) {
-          await this.query(
-            `query { getUser(args: { _id: "${creator}" }) { _id, data { name, followers, profileImg, coverImg } } }`,
-            r => {
-              creator = JSON.parse(r.responseText)["data"]["getUser"];
-              creators.push({
-                _id: creator._id,
-                name: creator["data"]["name"],
-                followers: creator["data"]["followers"].length,
-                profileImg: creator["data"]["profileImg"],
-                coverImg: creator["data"]["coverImg"],
-              });
-            }
-          );
-        }
-        data["creators"] = creators;
-
-        this.explore = data;
-        console.log(data);
-      }
-    );
-  }
+  async getExplore() {}
 }
