f0c461813c7b72c37eab83bdc5d090bfd9835d3d
Yogesh Aggarwal <developeryogeshgit@gmail.com>
2020-05-13 12:18:25 +0530

    Added route update to navbar

--- src/app/components/sidebar/sidebar.component.ts
+++ src/app/components/sidebar/sidebar.component.ts
@@ -1,12 +1,52 @@
 import { Component, OnInit } from "@angular/core";
+import { Router, NavigationEnd } from "@angular/router";
+
+interface routes {
+  home: boolean;
+  trending: boolean;
+  explore: boolean;
+}
 
 @Component({
   selector: "app-sidebar",
   templateUrl: "./sidebar.component.html",
-  styleUrls: ["./sidebar.component.scss"]
+  styleUrls: ["./sidebar.component.scss"],
 })
 export class SidebarComponent implements OnInit {
-  constructor() {}
+  routes: routes = {
+    home: false,
+    explore: false,
+    trending: false,
+  };
+  constructor(private router: Router) {}
 
-  ngOnInit(): void {}
+  setRoute(name: string) {
+    for (let route in this.routes) {
+      this.routes[route] = route == name ? true : false;
+    }
+  }
+
+  ngOnInit(): void {
+    this.router.events.subscribe((r) => {
+      if (r instanceof NavigationEnd) {
+        let end = r.url.split("/")[1];
+
+        switch (end) {
+          case "":
+            this.setRoute("home");
+            break;
+          case "explore":
+            this.setRoute("explore");
+            break;
+          case "trending":
+            this.setRoute("trending");
+            break;
+
+          default:
+            this.setRoute("");
+            break;
+        }
+      }
+    });
+  }
 }
